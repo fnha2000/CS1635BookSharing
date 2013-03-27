51e2da0c29b085788099192bee1670618e0a093b
blinskey <blinskey@gmail.com>
2013-03-26 20:49:38 -0400

    Fixed Buy screen

--- src/cs1635/group/booksharing/BuyActivity.java
+++ src/cs1635/group/booksharing/BuyActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -17,31 +18,35 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 public class BuyActivity extends Activity {
+	Context thisContext = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		thisContext = this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_buy);
 
 		EditText searchField = (EditText) findViewById(R.id.search_field);
 		
-		// Create layout for list
-        final LinearLayout listLayout = new LinearLayout(this);
-        listLayout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout parentLayout = (LinearLayout)this.findViewById(R.id.results_list);
-        parentLayout.addView(listLayout);
+		// Get list layout ID
+        final LinearLayout listLayout = (LinearLayout)this.findViewById(R.id.results_list);
         
         // Create layout for item
      	final LinearLayout itemLayout = new LinearLayout(this);
-     	listLayout.setOrientation(LinearLayout.HORIZONTAL);
+     	itemLayout.setOrientation(LinearLayout.HORIZONTAL);
+     	
         
      	// Create image
         final ImageView image = new ImageView(this);
         int resID = getResources().getIdentifier("android_text_photo", "drawable", getPackageName());	// Get resource ID of image
         image.setImageResource(resID);
+        image.setAdjustViewBounds(true);
+        image.setMaxHeight(200);
+        image.setMaxWidth(200);
         
         // Create nested layout
         final LinearLayout nestedLayout = new LinearLayout(this);
-        listLayout.setOrientation(LinearLayout.VERTICAL);
+        nestedLayout.setOrientation(LinearLayout.VERTICAL);
         
         // Create text labels
         final TextView title = new TextView(this);
@@ -52,19 +57,22 @@ public class BuyActivity extends Activity {
         
         // Create button
         final Button buyButton = new Button(this);
-        buyButton.setText(getResources().getString(R.string.buy));
+        buyButton.setText(getResources().getString(R.string.buy));       
+        buyButton.setOnClickListener(buyButtonListener);
 		
         searchField.setOnEditorActionListener(new OnEditorActionListener() {
         	@Override
         	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         		boolean handled = false;
     			
-    			if (actionId == EditorInfo.IME_ACTION_DONE) {
-        			
+    			if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+    				handled = true;
+    				
         			// Close keyboard
         			InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
     				imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-        			handled = true;
+        			
+        			// Erase text
         			
         			// Display search results
 	    			listLayout.addView(itemLayout);
@@ -94,5 +102,12 @@ public class BuyActivity extends Activity {
 		startActivity(intent);
 	}
     
+	OnClickListener buyButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			Intent intent = new Intent(thisContext, BuyBookDetailsActivity.class);
+			startActivity(intent);
+		}
+	};
 	
 }
